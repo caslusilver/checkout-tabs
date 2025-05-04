da1c7653c7446563aaefe4bf95cc24a8af8c9c82
caslusilver <lucaszm.crg@gmail.com>
2025-05-04 12:31:31 -0300

    insert uri e primaty branch

--- checkout-tabs.php
+++ checkout-tabs.php
@@ -4,6 +4,8 @@
  * Description: Converte o checkout em etapas (era seu snippet).
  * Version:     0.1.0
  * Author:      @casluads
+ * GitHub Plugin URI: caslusiver/checkout-tabs
+ * Primary Branch: main
  * License:     GPL-2.0+
  * Text Domain: checkout-tabs
  * Domain Path: /languages
